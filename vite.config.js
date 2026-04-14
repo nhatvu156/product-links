9de95be296d3a4c41d253cf56d2218595ef33ee3
nhatvu156 <nhatvu156@gmail.com>
2026-04-14 18:37:04 +0700

    Update base for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/product-links/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
